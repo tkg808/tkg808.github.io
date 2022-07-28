f64ac6b6e257bed9a57fa3dd5a82464548a02392
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-07-27 17:04:14 -0700

    Add test for landing page

---
+++ test/landingPageTest.js
@@ -0,0 +1,28 @@
+const { Builder, By, Key } = require("selenium-webdriver");
+const should = require("chai").should();
+
+describe("landing container renders", function ()
+{
+  it("successfully renders landing container", async function ()
+  {
+    const driver = await new Builder().forBrowser("chrome").build();
+
+    await driver.get("https://tkg808.github.io");
+
+    const landingName = await driver
+      .findElement(By.className("landing-name"))
+      .getText()
+      .then((value) => value);
+
+    landingName.should.equal("Tyson Gomes");
+
+    const brand = await driver
+      .findElement(By.className("brand"))
+      .getText()
+      .then((value) => value);
+
+    brand.should.equal("Solution in hand, you in mind.");
+
+    await driver.quit();
+  });
+});
